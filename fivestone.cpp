e260b0d0650ceca347f39aab2facbede11cf8a7e
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-05-08 20:09:49 +0900

    can count stone

--- fivestone.cpp
+++ fivestone.cpp
@@ -45,10 +45,10 @@ enum direction{
 };
 
 int directions[][2] = {
-  {-1,0},//DIRECTION_UP
-  {-1,1},//DIRECTION_RIGHT_UP
-  {0,1},//DIRECTION_RIGHT
-  {1,1},//DIRECTION_RIGHT_DOWN
+  {0,-1},//DIRECTION_UP
+  {1,-1},//DIRECTION_RIGHT_UP
+  {1,0}, //DIRECTION_RIGHT
+  {1,1}, //DIRECTION_RIGHT_DOWN
 };
 
 int game_start();
@@ -56,6 +56,7 @@ int set_init_board();
 int scan_position();
 int check_position(string _x,string _y);
 int set_board();
+int connected_stone_count(int _x,int _y);
 int judge_board();
 int game_end();
 
@@ -63,7 +64,7 @@ int deal;
 int turn = STONE_BLACK;
 string x,y;
 bool Right_position = true;
-int int_x,int_y;
+int num_x,num_y;
 
 int main(){
   deal = GAME_START;
@@ -81,8 +82,9 @@ int main(){
     case CHECK_POSITION :
       deal = check_position(x,y);
       break;
-      case SET_BOARD :
+    case SET_BOARD :
       deal = set_board();
+      cout << "その石と" << connected_stone_count(num_x,num_y) << "個の石がつながっています\n\n";
       break;
       /*case JUDGE_BOARD :
       deal = judge_board();
@@ -95,6 +97,7 @@ int main(){
     }
   }
 }
+
 int game_start(){
   for(int a = 0;a < SIZE;a++){
     for(int b = 0;b < SIZE;b++){
@@ -122,7 +125,7 @@ int scan_position(){
     cout << "Can't Put♡\n\n";
     Right_position = true;
   }
-  cout << "Player" << turn + 1 << " decide stone position\n"
+  cout << "Player" << turn + 1 << " decide stone's position\n"
        << "x :";
   cin >> x;
   cout << "y :";
@@ -132,15 +135,15 @@ int scan_position(){
 }
 
 int check_position(string _x,string _y){
-  int_x = (int)(_x[0] - '0'),int_y = (int)(_y[0] - '0');
+  num_x = (int)(_x[0] - '0'),num_y = (int)(_y[0] - '0');
   if(_x[1] != '\0' || _y[1] != '\0') Right_position = false;
-  else if(0 > int_x ||10 < int_x || 0 > int_y ||10 < int_y ) Right_position = false;
-  else if(stone_posi[int_y - 1][int_x - 1] != stone_type[STONE_NONE]) Right_position = false;
+  else if(0 > num_x ||SIZE < num_x || 0 > num_y ||SIZE < num_y ) Right_position = false;
+  else if(stone_posi[num_y - 1][num_x - 1] != stone_type[STONE_NONE]) Right_position = false;
   return Right_position? SET_BOARD : SCAN_POSITION;  
 }
 
 int set_board(){
-  stone_posi[int_y - 1][int_x - 1] = stone_type[turn];
+  stone_posi[num_y - 1][num_x - 1] = stone_type[turn];
   cout << "   1 2 3 4 5 6 7 8 9 (x)\n"; 
   for(int a = 0;a < SIZE;a++){
     cout << ' '<< a + 1;
@@ -150,12 +153,33 @@ int set_board(){
     cout << '\n';
   }
   cout << "(y)\n\n\n";
-  return JUDGE_BOARD;
+  return SCAN_POSITION;
+}
+
+int connected_stone_count(int _x,int _y){
+  int connected_stone_number = 0,max_count = 0;
+  for(int a = DIRECTION_UP;a < DIRECTION_MAX;a++){
+    connected_stone_number = 0;
+    for(int _X = _x,_Y = _y;0 <= _X && _X <= SIZE && 0 <=  _Y && _Y <= SIZE;
+        _X += directions[a][0],_Y += directions[a][1]){
+      if(stone_posi[_Y - 1][_X - 1] != stone_type[turn]) break; 
+      connected_stone_number++;
+    }
+    connected_stone_number--;
+    for(int _X = _x,_Y = _y;0 <= _X && _X <= SIZE && 0 <=  _Y && _Y <= SIZE;
+        _X -= directions[a][0],_Y -= directions[a][1]){
+      if(stone_posi[_Y - 1][_X - 1] != stone_type[turn]) break;
+      connected_stone_number++;
+    }
+    max_count = (connected_stone_number > max_count)? connected_stone_number : max_count;
+  }
+  return max_count;
 }
 /*
 int judge_board(){
   return GAME_END;
 }
+/*
 int game_end(){
   return DEAL_MAX;
 }
