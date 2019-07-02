530bd7be3d5403b3af3413f469c98a1ba60cdf10
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-07-02 18:50:45 +0900

    bold char

--- battlegame/src/game_master.cpp
+++ battlegame/src/game_master.cpp
@@ -3,8 +3,11 @@
 #include <ncurses.h>
 
 void Master::show_damage(const Person& emperor,const Person& brave) {
-  if(!brave.get_turn())
+  if(!brave.get_turn()){
+    attron(A_BOLD);
     mvprintw(20, 30, "brave attacked! %2d damages",brave.get_damage());
+    attroff(A_BOLD);
+  }
   else
     mvprintw(20, 30, "emperor attacked! %2d damages",emperor.get_damage());
 }
