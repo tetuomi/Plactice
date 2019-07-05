6a5f0096b303feca4864195ff652468b465ba96c
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-07-05 18:57:15 +0900

    give information to action class

--- battlegame/include/person.hpp
+++ battlegame/include/person.hpp
@@ -1,27 +1,23 @@
 #ifndef PERSON_HPP
 #define PERSON_HPP
-#include <iostream>
-#include "game_master.hpp"
 
-class Master;
-
-class Person {
-private:
-  int max_hp;
+typedef struct {
   int hp;
   int mp;
   int atack;
   int damage;
   int recovery;
+} status;
+
+class Person {
+private:
+  status _status;
+  int max_hp;
 public:
   Person(int _hp,int _atack,int _mp);
-  const int get_damage() const;
-  const int get_hp() const;
-  const int get_mp() const;
-  const int get_recovery() const;
-  friend void brave_atack(Person& person,Person& brave);
-  friend void emperor_atack(Person& person,Person& brave);
-  void hp_recovery(Person& person);
+  status get_status() const;
+  int get_max_hp() const;
+  status& set_status();
 };
-  
+
 #endif
--- battlegame/src/person.cpp
+++ battlegame/src/person.cpp
@@ -1,53 +1,20 @@
 #include "../include/person.hpp"
-#include <iostream>
-#include <iomanip>
-#include <random>
-#define MP 10
-#define RECOVERY 12
 
 Person::Person(int _hp,int _atack,int _mp) {
   max_hp = _hp;
-  hp = _hp;
-  mp = _mp;
-  atack = _atack;
+  _status.hp = _hp;
+  _status.mp = _mp;
+  _status.atack = _atack;
 }
 
-const int Person::get_damage() const {
-  return damage;
+status Person::get_status() const {
+  return _status;
 }
 
-const int Person::get_hp() const {
-  return hp;
-}
+int Person::get_max_hp() const {
+  return max_hp;
+ }
 
-const int Person::get_mp() const {
-  return mp;
-}
-
-const int Person::get_recovery() const {
-  return recovery;
-}
-
-std::random_device rnd;
-std::mt19937 mt(rnd());
-  
-void brave_atack(Person& emperor,Person& brave) {
-    brave.damage = brave.atack + mt()%brave.atack;
-    emperor.hp -= brave.damage;
-}
-
-void emperor_atack(Person& emperor,Person& brave) {
-    emperor.damage = emperor.atack + mt()%emperor.atack;
-    brave.hp -= emperor.damage;
-}
-
-void Person::hp_recovery(Person& person) {
-  person.mp -= MP;
-  recovery = RECOVERY + (mt() % RECOVERY);
-  person.hp += recovery;
-
-  if(person.hp >= person.max_hp) {
-    recovery -= (person.hp - person.max_hp);
-    person.hp = person.max_hp;
-  }
+status& Person::set_status() {
+  return _status;
 }
