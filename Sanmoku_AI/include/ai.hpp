7ba1fe1d9ee8e781296c198e343a7d5e31869d9a
tetuomi <kondo.tetta851@mail.kyutech.jp>
2020-04-03 21:44:06 +0900

    Add Qlearning ai

--- Sanmoku_AI/include/ai.hpp
+++ Sanmoku_AI/include/ai.hpp
@@ -2,12 +2,28 @@
 #define AI_HPP
 
 #include "player.hpp"
+#include <vector>
+
+enum class Mode
+{
+    QL,
+    RANDOM
+};
 
 class AI : public Player
 {
+private:
+    std::vector<std::vector<float>> q_table;
+    float epsilon;
+    
 public:
     AI();
+    std::size_t find_q_row(std::size_t before_hands) const;
     std::size_t get_hand() override;
+    void update_q_table(bool end_flag, float reward);
+    void set_epsilon(float ep);
+    void init_param();
+    void erase_choosable_hands(std::size_t hand);
 };
 
 
--- Sanmoku_AI/src/ai.cpp
+++ Sanmoku_AI/src/ai.cpp
@@ -3,29 +3,108 @@
 #include <random>
 #include <algorithm>
 #include <iostream>
+#include <cmath>
 
-std::random_device rnd;
-std::mt19937 mt(rnd());
-std::uniform_int_distribution<int> dist(1, 9);
+#define ETA 0.1
+#define GAMMA 0.9
 
 AI::AI()
-    :Player()
+    : Player()
+    , q_table(19683, std::vector<float>(9, 0.0)) // 19683 = 3^9
+    , epsilon{0.5}
 {}
+
+std::size_t AI::find_q_row(std::size_t before_hands) const
+{
+    // before_hand = 0 or 1
+    
+    std::size_t row_index = 0, coef = 0;
+    for (std::size_t i{0};i < chosen_hands.size() - before_hands;++i)
+    {
+        if (i % 2 == 0)
+        {
+            coef = 1;
+        }
+        else
+        {
+            coef = 2;
+        }
+        row_index += pow(3, chosen_hands[i] - 1) * coef;
+    }
+
+    return row_index;
+}
 
 std::size_t AI::get_hand()
 {
     std::size_t hand;
-    while (true)
+    if (epsilon_dist(mt) < epsilon)
     {
-        hand = dist(mt);
-        
-        auto result = std::find(chosen_hands.begin(), chosen_hands.end(), hand);
-
-        if (result == chosen_hands.end())
+        while (true)
         {
-            break;
+            hand = dist(mt);
+        
+            auto result = std::find(chosen_hands.begin(), chosen_hands.end(), hand);
+
+            if (result == chosen_hands.end())
+            {
+                break;
+            }
         }
     }
+    else
+    {
+        std::size_t row_index{find_q_row(0)};
+        hand = *max_element(choosable_hands.begin(), choosable_hands.end());
+        for (std::size_t i{0}; i < 9;++i)
+        {
+            if (choosable_hands[i] &&
+                q_table[row_index][choosable_hands[i] - 1] > q_table[row_index][hand - 1])
+            {
+                hand = choosable_hands[i];
+            }
+        }
+    }
+
     return hand;
 }
 
+
+
+
+void AI::update_q_table(bool end_flag, float reward)
+{
+    std::size_t before_row_index{find_q_row(1)};
+    std::size_t row_index{find_q_row(0)};
+    std::size_t column_index{chosen_hands.back() - 1};
+
+    if (end_flag)
+    {
+        q_table[before_row_index][column_index] = q_table[before_row_index][column_index]
+            + ETA * (reward - q_table[before_row_index][column_index]);
+    }
+    else
+    {
+        q_table[before_row_index][column_index] = q_table[before_row_index][column_index]
+            + ETA * (reward +
+                     GAMMA * (*max_element(q_table[row_index].begin(), q_table[row_index].end()))
+                     - q_table[before_row_index][column_index]);
+    }
+}
+
+void AI::set_epsilon(float ep)
+{
+    epsilon = ep;
+}
+
+void AI::init_param()
+{
+    for (std::size_t i{0};i < 9; ++i)
+    {
+        choosable_hands[i] = i + 1;
+    }
+
+    chosen_hands = std::vector<std::size_t>();
+
+    epsilon = 0.5;
+}
