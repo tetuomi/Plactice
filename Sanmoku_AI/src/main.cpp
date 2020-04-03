95550da16a88cc852e844e0da5d68228e2423c23
tetuomi <kondo.tetta851@mail.kyutech.jp>
2020-04-03 21:45:39 +0900

    QL ai vs Random ai

--- Sanmoku_AI/src/main.cpp
+++ Sanmoku_AI/src/main.cpp
@@ -1,26 +1,44 @@
 #include "person.hpp"
 #include "ai.hpp"
 #include "game_master.hpp"
+#include "random.hpp"
 
 int main ()
 {
-    Person person;
     AI ai;
-    GameMaster master;
+    Random random;
 
-    while (true)
+    float init_epsilon = 0.5, epsilon;
+    std::size_t episode = 5,reward = 0;
+    bool end_flag;
+    
+    for (std::size_t i{0};i < episode;++i)
     {
-        master.draw();                
-
-        if (master.game_end())
-        {
-            break;
+        GameMaster master;
+        epsilon = init_epsilon * (episode - i) / episode;
+        ai.set_epsilon(epsilon);
+        while (true)
+        {   
+            master.draw();             
+            master.game(ai, random); // (先行、後攻)
+            
+            end_flag = master.game_end();
+            if (end_flag)
+            {
+                master.draw();
+                reward = master.decide_reward();
+                ai.update_q_table(end_flag, reward);
+                break;
+            }
+            if (master.get_turn() != 1)
+            {
+                ai.update_q_table(end_flag, reward);
+            }
         }
         
-        master.game(person, ai);
+        master.result();
+        ai.init_param();
+        random.init_param();
     }
-
-    master.result();
-    
     return 0;
 }
